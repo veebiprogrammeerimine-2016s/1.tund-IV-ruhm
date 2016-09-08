2e33e26b0bc722dce6aaa4e760d1b9745f407ab2
Romil Robtsenkov <romil@greeny.cs.tlu.ee>
2016-09-08 15:18:20 +0300

    my first lines of code

---
+++ first.php
@@ -0,0 +1,14 @@
+<?php
+
+	$firstName = "Romil";
+	$lastName = "Rõbtšenkov";
+
+	// käsk echo trükib välja muutuja väärtus
+
+	//echo $firstName;
+	//echo $lastName;
+	
+	echo $firstName." ".$lastName;
+	
+	
+?>
